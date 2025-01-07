c23fe6e394eca2702f9ce052a6ad948712798f1d
laksne <lukas.anne@epitech.eu>
2025-01-07 23:57:47 +0000

    fix formatter for C, C++, Haskell, Lua

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,18 @@
 return {
---   "nvimtools/none-ls.nvim",
---   config = function()
---     local null_ls = require("null-ls")
---     null_ls.setup({
---       sources = {
---         null_ls.builtins.formatting.stylua,
---         null_ls.builtins.formatting.prettier,
---         null_ls.builtins.formatting.shellharden,
---         null_ls.builtins.diagnostics.erb_lint,
---       },
---     })
---
---     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
---   end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -- null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.formatting.prettier,
+        -- null_ls.builtins.formatting.shellharden,
+        -- null_ls.builtins.diagnostics.erb_lint,
+        null_ls.builtins.formatting.clang_format, -- C, C++
+        null_ls.builtins.formatting.stylua, -- Lua
+        null_ls.builtins.formatting.ormolu, -- Haskell
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
