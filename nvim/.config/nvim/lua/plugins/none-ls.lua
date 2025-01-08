9cfb8657f3eb99037c2d4e1cb0fa6afaaefc11a4
laksne <lukas.anne@epitech.eu>
2025-01-08 01:32:42 +0000

    fix error with haskell formatter

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -4,13 +4,8 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        -- null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.formatting.shellharden,
-        -- null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.formatting.clang_format, -- C, C++
-        null_ls.builtins.formatting.stylua, -- Lua
-        null_ls.builtins.formatting.ormolu, -- Haskell
+        null_ls.builtins.formatting.stylua,   -- Lua
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
